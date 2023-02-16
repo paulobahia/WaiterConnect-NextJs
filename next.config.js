e451a578dbf865c9f09c116d3987c4d67bce82ad
Bahia <you@example.com>
2023-02-15 22:18:38 -0300

    Atualização para versão mais recente do Next

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ['page.tsx','api.ts','api.tsx']
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
